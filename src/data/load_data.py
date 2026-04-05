dc25b8d3b8c54d0f5bf53f597a7384a08bbf1a46
fayza shemsu <fafibintshem@gmail.com>
2026-04-05 23:32:08 +0300

    Day 2: Implemented data cleaning and imputation strategy

---
+++ src/data/load_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_data(path: str):
+    try:
+        df = pd.read_csv(path)
+        print("✅ Data loaded successfully")
+        return df
+    except Exception as e:
+        print("❌ Error loading data:", e)
